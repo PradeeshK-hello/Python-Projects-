1c797b651757c0a37f3d1850632cd5555af85e84
PradeeshK-hello <sp.pradeeshkumar@gmail.com>
2025-11-01 13:51:25 +0800

    Add files via upload

---
+++ StringOperations.py
@@ -0,0 +1,15 @@
+first_name="Pradeesh Kumar"
+last_name=" Sathish Prabu"
+full_name=first_name+last_name
+example="Haa"*5
+
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Full Name:", full_name)
+print("String Multiplied 5 times give this result:", example)
+
+word="Pradeesh"
+print("Length of String:", len(word))
+print("First letter of string:", word[0])
+print("Last letter of string:", word[7])
+print("String sliced:", word[0:3])
