a1a174338b48f532fb9403d66dae05f5eb6122d0
PradeeshK-hello <sp.pradeeshkumar@gmail.com>
2025-11-01 13:37:03 +0800

    Add files via upload

---
+++ PythonOperators.py
@@ -0,0 +1,17 @@
+num1=45
+num2=3
+print("Number 1:", num1)
+print("Number 2:", num2)
+print("Addition:", num1+num2)
+print("Subtraction:", num1-num2)
+print("Multiplication:", num1*num2)
+print("Division:", num1/num2)
+print("Floor Division:", num1//num2)
+print("Modulus:", num1%num2)
+print("Square:", num2**2)
+print("Square root:", num1**0.5)
+
+print("Equal ?", num1==num2)
+print("Not Equal?", num1!=num2)
+print("Greater Than?", num1>num2)
+print("Lesser Than ?", num1<num2)
